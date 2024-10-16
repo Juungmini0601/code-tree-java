3d348665f83de11df4c413efb9ad1170d25050c1
raon37910 <raon37910@gmail.com>
2024-10-17 04:44:43 +0900

    Title: 2개 이상의 알파벳, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241017/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(check(input) ? "No" : "Yes");
+    }
+
+    public static boolean check(String input) {
+        for(int i = 1; i < input.length() - 1; i++) {
+            if(input.charAt(i-1) != input.charAt(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
