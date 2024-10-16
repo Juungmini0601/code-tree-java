b332e8b7d6c923c246cf644bfd28386db99e0014
raon37910 <raon37910@gmail.com>
2024-10-17 03:13:40 +0900

    Title: 함수를 이용한 소수 판별, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 241017/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 241017/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -17,6 +17,8 @@ public class Main {
     }
 
     public static boolean isPrime(int number) {
+        if(number == 1) return false;
+        
         for(int i = 2; i < number; i++) {
             if(number % i == 0) return false;
         }
