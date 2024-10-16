c225d409d70098755e9c7c7c124b356ddcae14ad
raon37910 <raon37910@gmail.com>
2024-10-17 03:09:58 +0900

    Title: 함수를 이용한 369 게임, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241017/함수를 이용한 369 게임/369-games-using-functions.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int cnt = 0;
+
+        for(int i = a; i <= b; i++) {
+            if(i % 3 == 0 || contain369(i)) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    public static boolean contain369(int number) {
+        boolean ret = false;
+
+        while(number > 0) {
+            int num = number % 10;
+            
+            if(num == 3 || num == 6 || num == 9) {
+                return true;
+            }
+
+            number /= 10;
+        }
+
+        return ret;
+    }
+}
