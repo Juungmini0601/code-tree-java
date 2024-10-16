83eafbe2ef47b7389db3be02be05d1a6813a1667
raon37910 <raon37910@gmail.com>
2024-10-17 04:12:03 +0900

    Title: 두 정수 값 교환하기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241017/두 정수 값 교환하기/to-exchange-two-integer-values.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        IntWrapper a = new IntWrapper(sc.nextInt());
+        IntWrapper b = new IntWrapper(sc.nextInt());
+        swap(a, b);
+        System.out.println(a.value + " " + b.value);
+    }
+
+    public static void swap(IntWrapper a, IntWrapper b) {
+        int temp = a.value;
+        a.value = b.value;
+        b.value = temp;
+    }
+}
+
+class IntWrapper {
+    int value;
+
+    public IntWrapper (int value) {
+        this.value = value;
+    }
+}
