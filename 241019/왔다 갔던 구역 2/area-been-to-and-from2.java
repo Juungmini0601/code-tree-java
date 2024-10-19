db4ce9807c4a70d2161187bf4921d289017a0c16
raon37910 <raon37910@gmail.com>
2024-10-19 20:23:18 +0900

    Title: 왔다 갔던 구역 2, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241019/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cur = 1000;
+        int n = sc.nextInt();
+        int[] arr = new int[2001];
+
+        for(int i = 0; i < n; i++) {
+            int distance = sc.nextInt();
+            String dir = sc.next();
+
+            if(dir.equals("R")) {
+                while(distance > 0) {
+                    cur++;
+                    arr[cur]++;
+                    distance--;
+                }
+            } else {
+                while(distance > 0) {
+                    cur--;
+                    arr[cur]++;
+                    distance--;
+                }
+            }
+        }
+
+        int ans = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] >= 2) {
+                ans++;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
