e288815ec0c7de3237c82070a9352e542e2fb5cf
raon37910 <raon37910@gmail.com>
2024-10-19 20:18:33 +0900

    Title: 최대로 겹치는 구간, Time: 179ms, Memory: 10MB, Status: Passed - CodeTree

--- 241019/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 241019/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -11,7 +11,7 @@ public class Main {
             int x2 = sc.nextInt();
 
             for(int j = x1; j < x2; j++) {
-                arr[j]++;
+                arr[j+100]++;
             }
         }
 
