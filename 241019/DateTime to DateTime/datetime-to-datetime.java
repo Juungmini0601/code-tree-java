e1192147be49c8b78c743399c426a33ffe591621
raon37910 <raon37910@gmail.com>
2024-10-19 16:36:06 +0900

    Title: DateTime to DateTime, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241019/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int ans = getTotalMiniut(a - 11, b, c) - getTotalMiniut(0, 11, 11);
+        System.out.println(ans > 0 ? ans : -1);
+    }
+
+    public static int getTotalMiniut(int d, int h, int m) {
+        return d * 24 * 60 + h * 60 + m;
+    }
+}
