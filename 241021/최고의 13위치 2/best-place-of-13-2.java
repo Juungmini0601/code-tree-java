fb6957ce4eaaf2e845805cb01902d9936dd3051c
raon37910 <raon37910@gmail.com>
2024-10-21 04:32:09 +0900

    Title: 최고의 13위치 2, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 241021/최고의 13위치 2/best-place-of-13-2.java
+++ 241021/최고의 13위치 2/best-place-of-13-2.java
@@ -14,11 +14,16 @@ public class Main {
 
         int ans = 0;
 
-        for(int i = 0; i < n-1; i++) {
+        for(int i = 0; i < n; i++) {
             for(int j = 0; j < n - 2; j++) {
                 // ans = Math.max(ans, arr[i][j] + arr[i][j+1] + arr[i][j+2]);
-                for(int k = i + 1; k < n; k++) {
+                for(int k = i; k < n; k++) {
                     for(int l = 0; l < n - 2; l++) {
+                        if(
+                            (i == k && j == l) || (i == k && j == l + 1) || (i == k && j == l + 2) ||
+                            (i == k && j+1 == l) || (i == k && j+1 == l + 1) || (i == k && j+1 == l + 2) ||
+                            (i == k && j+2 == l) || (i == k && j+2 == l + 1) || (i == k && j+2 == l + 2)
+                        ) continue;
                         int sum1 = arr[i][j] + arr[i][j+1] + arr[i][j+2];
                         int sum2 = arr[k][l] + arr[k][l+1] + arr[k][l+2];
                         ans = Math.max(ans, sum1 + sum2);
