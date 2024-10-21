c24fc32576fd27211305bd0b8e376bb07badb24f
raon37910 <raon37910@gmail.com>
2024-10-21 16:45:27 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 175ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241021/한 가지로 열리는 자물쇠/one-way-lock.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[3];
+        
+        for(int i = 0; i < 3; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int cnt = 0;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                for(int k = 1; k <= n; k++) {
+                    if(Math.abs(i - arr[0]) <= 2 || Math.abs(j - arr[1]) <= 2|| Math.abs(k - arr[2]) <= 2) {
+                        cnt++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
