8d85498df8276ff5adcf9c656897e6584bbe80bf
raon37910 <raon37910@gmail.com>
2024-10-21 17:23:02 +0900

    Title: 개발자의 능력 2, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241021/개발자의 능력 2/ability-of-developer-2.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] scores = new int[6];
+        int total = 0;
+
+        for(int i = 0; i < 6; i++) {
+            scores[i] = sc.nextInt();
+            total += scores[i];
+        }
+        
+        int ans = Integer.MAX_VALUE;
+
+        for(int i = 0; i < 5; i++) {
+            for(int j = i+1; j < 6; j++) {
+                for(int k = 0; k < 5; k++) {
+                    if(i == k) {
+                        continue;
+                    }
+
+                    for(int l = k+1; l < 6; l++) {
+                        if(l == j) {
+                            continue;
+                        }
+
+                        int sum1 = scores[i] + scores[j];
+                        int sum2 = scores[k] + scores[l];
+                        int sum3 = total - sum1 - sum2;
+                        int maxValue = max(sum1, sum2, sum3);
+                        int minValue = min(sum1, sum2, sum3);
+                        ans = Math.min(ans, maxValue - minValue);
+                    }
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    public static int max(int a, int b, int c) {
+        int max = a;
+
+        if(max < b) {
+            max = b;
+        }
+
+        if(max < c) {
+            max = c;
+        }
+
+        return max;
+    }
+
+    public static int min(int a, int b, int c) {
+        int min = a;
+
+        if(min > b) {
+            min = b;
+        }
+
+        if(min > c) {
+            min = c;
+        }
+
+        return min;
+    }
+}
