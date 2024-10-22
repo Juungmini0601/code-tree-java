e4d5fd7b74e95eed861957fa323f85e4f8f1ef4b
raon37910 <raon37910@gmail.com>
2024-10-22 13:02:57 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241022/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = Integer.MAX_VALUE;
+        Point[] points = new Point[n];
+        
+        for(int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            points[i] = new Point(x, y);
+        }
+        
+
+        for(int i = 0; i < n; i++) {
+            int minX = Integer.MAX_VALUE;
+            int minY = Integer.MAX_VALUE;
+            int maxX = Integer.MIN_VALUE;
+            int maxY = Integer.MIN_VALUE;
+
+            for(int j = 0 ; j < n; j++) {
+                if(i == j) {
+                    continue;
+                }
+                Point p = points[j];
+                minX = Math.min(minX, p.x);
+                maxX = Math.max(maxX, p.x);
+                minY = Math.min(minY, p.y);
+                maxY = Math.max(maxY, p.y);
+            }
+
+            ans = Math.min(ans, (maxX - minX) * (maxY - minY));
+        }
+
+        System.out.println(ans);
+    }
+}
+
+class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
