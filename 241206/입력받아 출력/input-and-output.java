3d7b15d1fe27dc5e3a8122277972df29f95699f5
raon37910 <raon37910@gmail.com>
2024-12-06 01:03:14 +0900

    Title: 입력받아 출력, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241206/입력받아 출력/input-and-output.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int a;
+    static int b;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        b = Integer.parseInt(st.nextToken());
+
+        System.out.printf("%d %d\n", a, b);
+    }
+}
