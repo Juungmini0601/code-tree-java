37a644e2af6b25aee03403923eb135574c234910
raon37910 <raon37910@gmail.com>
2024-12-06 06:48:00 +0900

    Title: 날짜 변경하여 출력, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241206/날짜 변경하여 출력/print-date-with-different-format.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine(), ".");
+        int y = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        System.out.printf("%d-%d-%d", m, d, y);
+    }
+}
