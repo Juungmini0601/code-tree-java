dc554ef0d41843ad338c921fbf27a51bc3033e87
raon37910 <raon37910@gmail.com>
2024-12-08 05:52:03 +0900

    Title: 정수 입력받아 배수 출력, Time: 80ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241208/정수 입력받아 배수 출력/print-multiple-of-input.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int end = 5 * n;
+
+        for(int i = n; i <= end; i += n) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
