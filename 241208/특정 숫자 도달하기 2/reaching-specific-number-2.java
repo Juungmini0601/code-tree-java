b661cee44fe66f3d7a4f58466cad8947ec5a2d1c
raon37910 <raon37910@gmail.com>
2024-12-08 05:55:51 +0900

    Title: 특정 숫자 도달하기 2, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241208/특정 숫자 도달하기 2/reaching-specific-number-2.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+       
+        for(int i = n; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
