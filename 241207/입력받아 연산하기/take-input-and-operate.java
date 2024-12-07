41d380356c6d033bfc5e0e24ebd0aef33ea43022
raon37910 <raon37910@gmail.com>
2024-12-07 18:37:56 +0900

    Title: 입력받아 연산하기, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241207/입력받아 연산하기/take-input-and-operate.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int b = Integer.parseInt(st.nextToken());
+
+        a += 87;
+        b %= 10;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
