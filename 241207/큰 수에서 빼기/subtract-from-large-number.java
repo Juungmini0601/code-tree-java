42aba5ee7db9f69a373694d19a2eb430a1fc784c
raon37910 <raon37910@gmail.com>
2024-12-07 18:56:38 +0900

    Title: 큰 수에서 빼기, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241207/큰 수에서 빼기/subtract-from-large-number.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        int max = Math.max(a, b);
+        int min = Math.min(a, b);
+
+        System.out.println(max - min);
+    }
+}
