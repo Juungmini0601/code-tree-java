08cb74f81733117d385824f40d71e93131e9b617
raon37910 <raon37910@gmail.com>
2024-12-07 18:49:12 +0900

    Title: 합과 평균의 차, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241207/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int sum = a + b + c;
+        int avg = sum / 3;
+
+        System.out.println(sum);
+        System.out.println(avg);
+        System.out.println(sum - avg);
+    }
+}
