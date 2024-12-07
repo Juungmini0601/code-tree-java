ef772625cafe3a3982e2229b28cbceca3a63ac9c
raon37910 <raon37910@gmail.com>
2024-12-07 19:46:14 +0900

    Title: 삼항연산자 2, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241207/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        
+        String answer = a == 1 ? "t" : "f";
+        
+        System.out.println(answer);
+    }
+}
