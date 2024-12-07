6af90ee00d26ab904265f9a10d464ef64de51093
raon37910 <raon37910@gmail.com>
2024-12-07 19:15:26 +0900

    Title: 숫자의 조건 여부, Time: 85ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241207/숫자의 조건 여부/numbers-condition.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        
+        if (a >= 113) {
+            System.out.println("1");
+        }
+        else {
+            System.out.println("0");
+        }
+    }
+}
