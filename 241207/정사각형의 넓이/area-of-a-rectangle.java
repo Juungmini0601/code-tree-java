32be85dfe312a8005f0f1c8ea0eec0c0a4b42fce
raon37910 <raon37910@gmail.com>
2024-12-07 18:59:34 +0900

    Title: 정사각형의 넓이, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241207/정사각형의 넓이/area-of-a-rectangle.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        
+        int area = n * n;
+
+        System.out.println(area);
+
+        if(b < 5) {
+            System.out.println("tiny");
+        }
+    }
+}
