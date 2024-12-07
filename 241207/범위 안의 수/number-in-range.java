e1e3d1bdbdfec8ab110b3bfc593c3b361bb5dc93
raon37910 <raon37910@gmail.com>
2024-12-07 23:13:39 +0900

    Title: 범위 안의 수, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241207/범위 안의 수/number-in-range.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+       
+        if (a >= 10 && a <= 20) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
