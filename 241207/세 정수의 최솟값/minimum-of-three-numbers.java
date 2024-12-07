64fb9e1ec7c760206672716b8b61d64aedbc043b
raon37910 <raon37910@gmail.com>
2024-12-07 23:22:44 +0900

    Title: 세 정수의 최솟값, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241207/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int min = a;
+        
+        if(min > b) {
+            min = b;
+        }
+
+        if(min > c) {
+            min = c;
+        }
+
+        System.out.println(min);
+    }
+}
