3ef13e682789924acc971a223adafdca1271d681
raon37910 <raon37910@gmail.com>
2024-12-07 18:47:12 +0900

    Title: 세 수의 합과 평균 구하기, Time: 96ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241207/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int sum = a + b + c;
+        double avg = (double) sum / 3;
+
+        System.out.printf("%d %.1f\n", sum, avg);
+    }
+}
