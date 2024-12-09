6c35e7677628fc3d6cb003c2aa8d1cff770dbd0f
raon37910 <raon37910@gmail.com>
2024-12-09 18:58:42 +0900

    Title: 증가시키며 출력하기 2, Time: 83ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241209/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        
+        while(i <= 26) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
