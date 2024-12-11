871b667bc2eeb3cec8a68b1062df3eaa6db0db3f
raon37910 <raon37910@gmail.com>
2024-12-11 23:11:35 +0900

    Title: 특정 조건의 온전수 구하기, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241211/특정 조건의 온전수 구하기/print-onjeonsu.java
@@ -0,0 +1,44 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb;
+    static int n;
+    static int cnt;
+
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
+    }
+
+    public static void solve() {
+        sb = new StringBuilder();
+
+        for(int i = 1; i <= n; i++) {
+            if(i % 2 == 0) {
+                continue;
+            }
+
+            if(i % 10 == 5) {
+                continue;
+            }
+
+            if(i % 3 == 0 && i % 9 != 0) {
+                continue;
+            }
+
+            sb.append(i + " ");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void input() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+    }
+}
