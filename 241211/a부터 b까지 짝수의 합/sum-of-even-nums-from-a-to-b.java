ef9ff0d56f2dac5f182e1517fd108fbf23bf9b57
raon37910 <raon37910@gmail.com>
2024-12-11 20:24:35 +0900

    Title: a부터 b까지 짝수의 합, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241211/a부터 b까지 짝수의 합/sum-of-even-nums-from-a-to-b.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        int sum = 0;
+        
+        for(int i = a ; i <= b; i++) {
+            if(i % 2 == 0) {
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+}
