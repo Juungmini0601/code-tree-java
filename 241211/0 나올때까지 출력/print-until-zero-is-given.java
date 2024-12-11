2ab0d753fc58fe55ab9db48510708f5046af7f05
raon37910 <raon37910@gmail.com>
2024-12-11 23:40:35 +0900

    Title: 0 나올때까지 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241211/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,36 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws Exception {
+        // input();
+        solve();
+    }
+
+    public static void solve() throws Exception {
+        sb = new StringBuilder();
+
+        while(true) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            if(n == 0) {
+                break;
+            }
+
+            sb.append(n).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void input() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+    }
+}
