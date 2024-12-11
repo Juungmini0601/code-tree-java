caee77ee1408ca80fc8e172eec375a8bda3e23a2
raon37910 <raon37910@gmail.com>
2024-12-11 23:35:47 +0900

    Title: 1까지 나누기, Time: 89ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241211/1까지 나누기/divide-by-1.java
@@ -0,0 +1,41 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb;
+    static int n;
+    static int num = 1;
+    static int cnt = 0;
+
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
+    }
+
+    public static void solve() {
+        sb = new StringBuilder();
+
+        while(true) {
+            n /= num;
+
+            if(n < 1) {
+                break;
+            }
+
+            num++;
+        }
+
+        sb.append(num);
+
+        System.out.println(sb);
+    }
+
+    public static void input() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+    }
+}
