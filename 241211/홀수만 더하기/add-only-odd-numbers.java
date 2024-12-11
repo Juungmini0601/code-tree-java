59ad2419d36f37280f6e8c7aaffd4f6b1a120a40
raon37910 <raon37910@gmail.com>
2024-12-11 20:17:13 +0900

    Title: 홀수만 더하기, Time: 93ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241211/홀수만 더하기/add-only-odd-numbers.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int sum = 0;
+
+        for(int i = 1 ; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+
+            if(num % 2 == 1 && num % 3 == 0) {
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+}
