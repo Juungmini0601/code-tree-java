43671e237aadb992e44de411e78c979cfd2e0ec4
raon37910 <raon37910@gmail.com>
2024-12-13 18:57:02 +0900

    Title: 별표 출력하기 3, Time: 79ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241213/별표 출력하기 3/print-star-3.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n; 
+
+    public static void main(String[] args) throws Exception {
+        input();
+        solution();
+    }
+
+    public static void solution() {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 1; i <= n; i++) {
+            // 공백 출력
+            for(int j = 1; j <= i-1; j++) {
+                sb.append("  ");
+            }
+            // 별 출력
+            for(int j = 1; j <= 2 * (n - i) + 1; j++) {
+                sb.append("* ");
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void input() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+    }
+
+}
