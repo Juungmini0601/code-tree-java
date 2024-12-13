12337855c20dfa2128832b9abcfac06d7d4c8912
raon37910 <raon37910@gmail.com>
2024-12-13 20:33:28 +0900

    Title: 규칙을 찾아 출력하기, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241213/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n; 
+
+    public static void main(String[] args) throws Exception {
+        input();
+        solution();
+    }
+
+    public static void solution() {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(i == 1 || i == n || j == 1 || j == n) {
+                    sb.append("* ");
+                }
+                else {
+                    if(j <= i - 1) {
+                        sb.append("* ");
+                    }
+                    else {
+                        sb.append("  ");
+                    }
+                }
+            }
+            
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void input() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+    }
+
+}
