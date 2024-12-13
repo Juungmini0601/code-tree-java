9147c565907f197b8e14c332f0113340495de9d4
raon37910 <raon37910@gmail.com>
2024-12-14 06:26:14 +0900

    Title: 정상적인 구구단, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241214/정상적인 구구단/normal-multiple.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n; 
+
+    public static void main(String[] args) throws Exception {
+        input();
+        solution();
+    }
+
+    public static void solution() {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                sb.append(String.format("%d * %d = %d", i, j, i * j));
+                
+                if(j != n) {
+                    sb.append(", ");
+                }
+            }
+
+            sb.append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+
+    public static void input() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+    }
+
+}
