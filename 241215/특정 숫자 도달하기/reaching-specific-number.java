212b3bb08df057387445580327dc01285bb71ffd
raon37910 <raon37910@gmail.com>
2024-12-15 22:12:23 +0900

    Title: 특정 숫자 도달하기, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241215/특정 숫자 도달하기/reaching-specific-number.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+    public static int[] arr = new int[10];
+
+    public static void main(String[] args) throws Exception {
+        input();
+        solution();
+    }
+
+    public static void solution() throws Exception {
+        StringBuilder sb = new StringBuilder();
+
+        int sum = 0;
+        int cnt = 0;
+        
+        for(int i = 0; i < 10; i++) {
+            if(arr[i] >= 250) {
+                break;
+            }
+
+            sum += arr[i];
+            cnt++;
+        }
+        
+        double avg = (double) sum / cnt;
+        sb.append(String.format("%d %.1f", sum, avg));
+        System.out.println(sb);
+    }
+
+    public static void input() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        
+        for(int i = 0; i < 10; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+}
