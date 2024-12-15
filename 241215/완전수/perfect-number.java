97f24945a89e05e1aba523722903066ad8e1f53e
raon37910 <raon37910@gmail.com>
2024-12-15 21:53:59 +0900

    Title: 완전수, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241215/완전수/perfect-number.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+    public static int start;
+    public static int end;
+
+    public static void main(String[] args) throws Exception {
+        input();
+        solution();
+    }
+
+    public static void solution() throws Exception {
+        StringBuilder sb = new StringBuilder();
+        int cnt = 0;
+
+        for(int i = start; i <= end; i++) {
+           if(check(i)) {
+             cnt++;
+           }
+        }
+        
+        sb.append(cnt);
+        System.out.println(sb);
+    }
+
+    public static boolean check(int x) {
+        int sum = 0;
+        
+        for(int i = 1; i < x; i++) {
+            if(x % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == x;
+    }
+
+    public static void input() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
+    }
+}
