35c296e1a45b0afb8f23a3f88d049d9e4511034d
raon37910 <raon37910@gmail.com>
2024-12-15 20:58:14 +0900

    Title: 숫자 피라미드 2, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241215/숫자 피라미드 2/number-pyramid-2.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+    public static int n;
+
+    public static void main(String[] args) throws Exception {
+        input();
+        solution();
+    }
+
+    public static void solution() {
+        StringBuilder sb = new StringBuilder();
+        int cnt = 1;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                sb.append(cnt + " ");
+                cnt++;
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void input() throws Exception {
+        n = Integer.parseInt(br.readLine());
+    }
+}
