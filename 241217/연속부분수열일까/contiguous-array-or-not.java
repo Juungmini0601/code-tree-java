8a66b209d709ecbffd136f054a4f917c2f91d797
raon37910 <raon37910@gmail.com>
2024-12-17 21:37:16 +0900

    Title: 연속부분수열일까, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

--- 241217/연속부분수열일까/contiguous-array-or-not.java
+++ 241217/연속부분수열일까/contiguous-array-or-not.java
@@ -24,7 +24,7 @@ public class Main {
             return;
         }
 
-        for(int i = 0; i < n1 - n2; i++) {
+        for(int i = 0; i <= n1 - n2; i++) {
             if(check(i)) {
                 System.out.println("Yes");
                 return;
