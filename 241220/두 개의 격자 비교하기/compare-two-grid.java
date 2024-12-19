6f9a5033688c0fec515577b330cfc32365272758
raon37910 <raon37910@gmail.com>
2024-12-20 06:49:03 +0900

    Title: 두 개의 격자 비교하기, Time: 91ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241220/두 개의 격자 비교하기/compare-two-grid.java
@@ -0,0 +1,58 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+    public static int n;
+    public static int m;
+    public static int[][] arr1;
+    public static int[][] arr2;
+
+    public static void main(String[] args) throws Exception {
+        input();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(arr1[i][j] == arr2[i][j]) {
+                    sb.append(0).append(" ");
+                } else {
+                    sb.append(1).append(" ");
+                }
+            }
+            
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void input() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr1 = new int[n][m];
+        arr2 = new int[n][m];
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for(int j = 0; j < n; j++) {
+                arr1[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for(int j = 0; j < n; j++) {
+                arr2[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+}
