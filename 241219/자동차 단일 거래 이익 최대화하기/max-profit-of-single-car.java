34b000932288182fd249e2e079348411f61d3e0c
raon37910 <raon37910@gmail.com>
2024-12-19 23:12:29 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 86ms, Memory: 9MB, Status: Runtime Error - CodeTree

---
+++ 241219/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -0,0 +1,37 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+    public static int n;
+    public static int[] arr = new int[10];
+
+    public static void main(String[] args) throws Exception {
+        input();
+        int maxDiff = 0;
+
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int dif = arr[j] - arr[i];
+                if(dif >= 0) {
+                    maxDiff = Math.max(dif, maxDiff);
+                }
+            }
+        }
+
+        System.out.println(maxDiff);
+    }
+
+    public static void input() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+}
