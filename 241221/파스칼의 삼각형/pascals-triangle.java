ddc36cdd53054efb2c6afce133f813f3b8e21a62
raon37910 <raon37910@gmail.com>
2024-12-21 13:09:36 +0900

    Title: 파스칼의 삼각형, Time: 88ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241221/파스칼의 삼각형/pascals-triangle.java
@@ -0,0 +1,33 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Main {
+
+    public static int n;
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n+1][n+1];
+        StringBuilder sb = new StringBuilder();
+
+        for(int row = 1; row <= n; row++) {
+            arr[row][1] = 1;
+            arr[row][row] = 1;
+        }
+
+        for(int row = 2; row <= n; row++) {
+            for(int col = 2; col <= row; col++) {
+                arr[row][col] = arr[row-1][col-1] + arr[row-1][col];
+            }
+        }
+
+        for(int row = 1; row <= n; row++) {
+            for(int col = 1; col <= row; col++) {
+                sb.append(arr[row][col]).append(" ");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
