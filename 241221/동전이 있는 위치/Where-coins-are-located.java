e6c63d46c0c3e87b9eb3d0980de3823f609c1007
raon37910 <raon37910@gmail.com>
2024-12-21 13:43:26 +0900

    Title: 동전이 있는 위치, Time: 83ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241221/동전이 있는 위치/Where-coins-are-located.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st ;
+    public static int n;
+    public static int m;
+    public static int[][] arr;
+
+    public static void main(String[] args) throws Exception {
+        input();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                sb.append(arr[i][j]).append(" ");
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void input() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n+1][n+1];
+
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            arr[r][c] = 1;
+        }
+    }
+}
