eadc8eb0b51fa6e4a21d64bb9c7e6526f7577695
raon37910 <raon37910@gmail.com>
2024-12-22 15:12:14 +0900

    Title: 두 번째를 첫 번째로, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241222/두 번째를 첫 번째로/second-to-first.java
@@ -0,0 +1,25 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        String str = br.readLine();
+        char c1 = str.charAt(0);
+        char c2 = str.charAt(1);
+
+        char[] arr = str.toCharArray();
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == c2) {
+                arr[i] = c1;
+            }
+        }
+
+        System.out.println(String.valueOf(arr));
+    }
+}
