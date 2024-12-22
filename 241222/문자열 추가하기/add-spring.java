878363b8ce8b03e2c6d1e9f2cf344855942062f9
raon37910 <raon37910@gmail.com>
2024-12-22 14:39:59 +0900

    Title: 문자열 추가하기, Time: 85ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241222/문자열 추가하기/add-spring.java
@@ -0,0 +1,14 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        String str = br.readLine();
+        System.out.println(str + "Hello");
+    }   
+}
