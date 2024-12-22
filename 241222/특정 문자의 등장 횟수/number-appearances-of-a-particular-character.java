d454b3eaa6453919d1548b250eb7e680bf8dcb7d
raon37910 <raon37910@gmail.com>
2024-12-22 14:58:56 +0900

    Title: 특정 문자의 등장 횟수, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241222/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.java
@@ -0,0 +1,25 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        String str = br.readLine();
+        System.out.println(count(str, "ee") + " " + count(str, "eb"));
+    }
+
+    public static int count(String str, String target) {
+        int cnt = 0;
+        for(int i = 0; i < str.length() - target.length() + 1; i++) {
+            if(str.substring(i, i + target.length()).equals(target)) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+}
