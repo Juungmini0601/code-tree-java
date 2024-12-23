dfec63af130de40fc7b1e8ff4732534428ed1678
raon37910 <raon37910@gmail.com>
2024-12-23 14:37:49 +0900

    Title: 문자열 밀기, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241223/문자열 밀기/push-char.java
@@ -0,0 +1,15 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        String str = br.readLine();
+        str = str.substring(1) + str.substring(0, 1);
+        System.out.println(str);
+    }
+}
