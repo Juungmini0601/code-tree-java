d21ee20c842a395cfa22c3e4a7413ae38c6d0b7f
raon37910 <raon37910@gmail.com>
2024-12-25 11:04:38 +0900

    Title: 반복 출력하기, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241225/반복 출력하기/repeated-output.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            print();
+        }
+    }
+
+    public static void print() {
+        System.out.println("12345^&*()_");
+    }
+}
