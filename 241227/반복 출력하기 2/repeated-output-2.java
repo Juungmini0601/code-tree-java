30cd9c66a1297bd3a2363d040ed81140ce82712b
raon37910 <raon37910@gmail.com>
2024-12-27 13:00:22 +0900

    Title: 반복 출력하기 2, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

--- 241227/반복 출력하기 2/repeated-output-2.java
+++ 241227/반복 출력하기 2/repeated-output-2.java
@@ -1,14 +1,24 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        print(0);
+    
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+    public static int n;
+
+    public static void main(String[] args) throws Exception {
+        n = Integer.parseInt(br.readLine());
+        print(0);    
     }
 
     public static void print(int i) {
-        if(i >= 4) {
+        if(i >= n) {
             return;
         }
 
         System.out.println("HelloWorld");
-        print(i+1);
+        print(i + 1);
     }
 }
