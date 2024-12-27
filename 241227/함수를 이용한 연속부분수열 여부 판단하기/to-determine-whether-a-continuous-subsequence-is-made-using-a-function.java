dabf1a819566356aeec52928e7b91d7dbda2beb0
raon37910 <raon37910@gmail.com>
2024-12-27 10:48:40 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 80ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241227/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,54 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+
+        int[] arr1 = new int[n1];
+        int[] arr2 = new int[n2];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n1; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n2; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < n1; i++) {
+            if(contains(arr1, arr2, i)) {
+                System.out.println("Yes");
+                return;
+            }
+        }
+
+        System.out.println("No");
+    }
+
+    public static boolean contains(int[] arr1, int[] arr2, int arr1Index) {
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+
+        if(arr1Index + n2 >= n1) return false;
+
+        for(int i = 0; i < n2; i++) {
+            if(arr1[arr1Index + i] != arr2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
