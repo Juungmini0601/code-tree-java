c6513445f6ad9fd692897769fba9aa4142c817d0
raon37910 <raon37910@gmail.com>
2025-01-01 10:23:30 +0900

    Title: 훌륭한 점프, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

--- 250101/훌륭한 점프/great-jump.java
+++ 250101/훌륭한 점프/great-jump.java
@@ -1,6 +1,7 @@
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
 
@@ -41,7 +42,9 @@ public class Main {
             }   
         }
 
-        if(arr[cnt-1] != n - 1) {
+        // System.out.println(maxValue + ":/ " + Arrays.toString(selected));
+
+        if(selected[cnt-1] != n - 1) {
             return false;
         }
 
@@ -50,7 +53,6 @@ public class Main {
                 return false;
             }
         }
-
         return true;
     }
 }
