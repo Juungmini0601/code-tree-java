e70af48998ff245b0c5369dc7619f3a69cd7f0b2
raon37910 <raon37910@gmail.com>
2025-01-05 08:05:12 +0900

    Title: 구간 잘 나누기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 250105/구간 잘 나누기/divide-sections-well.java
+++ 250105/구간 잘 나누기/divide-sections-well.java
@@ -1,70 +1,46 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static StringTokenizer st;
-    public static int n;
-    public static int m;
-    public static int[] arr;
-    public static int[] selected;
-    public static boolean[] visited;
-    public static int ans = Integer.MAX_VALUE;
-
-    public static void main(String[] args) throws Exception {
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        arr = new int[n];
-        visited = new boolean[n];
-        selected = new int[m-1];
-
-        st = new StringTokenizer(br.readLine());
+    static final int MAX_NUM = 10000;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
         
-        for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-
-        select(0);
-        System.out.println(ans);
-    }
-
-    public static void select(int cur) {
-        if(cur >= m - 1) {
-            int startIndex = 0;
-            int maxSum = Integer.MIN_VALUE;
-
-            for(int i = 0; i < selected.length; i++) {
-                int endIndex = selected[i];
-                maxSum = Math.max(maxSum, getSum(startIndex, endIndex));
-                startIndex = endIndex + 1;
+        
+        int answer = 0;
+        // 합의 최소값을 구간으로 검색한다.
+        for (int i = 1; i <= MAX_NUM; i++) {
+            boolean possible = true;
+            // 아무런 구간도 나누지 않으면 섹션은 1개이다.
+            int section = 1, sum = 0;
+            
+            for (int j = 0; j < n; j++) {
+                // 특정 원소가 현재 최대값보다 크다면 불가능한 경우이다.
+                if (arr[j] > i) {
+                    possible = false;
+                    break;
+                }
+                // 현재 원소가 포함되었을 때 구간이 안되면 섹션을 나눠준다.
+                if (arr[j] + sum > i) {
+                    sum = 0;
+                    section++;
+                }
+                
+                sum += arr[j];
             }
-
-            maxSum = Math.max(maxSum, getSum(startIndex, n-1));
-
-            ans = Math.min(ans, maxSum);
-            return;
+            
+            if (possible && section <= m) {
+                System.out.println(i);
+                break;
+            }
         }
-
-        for(int i = 0; i < n; i++) {
-            if(visited[i]) continue;
-            if(cur != 0 && selected[cur-1] > i) continue;
-            visited[i] = true;
-            selected[cur] = i;
-            select(cur + 1);
-            selected[cur] = 0;
-            visited[i] = false;
-        }
-    }
-
-    public static int getSum(int startIndex, int endIndex) {
-        int sum = 0;
-        for(int i = startIndex; i <= endIndex; i++) {
-            sum += arr[i];
-        }
-        return sum;
+        
+        sc.close();
     }
 }
