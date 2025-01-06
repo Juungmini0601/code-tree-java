7a193f71da5c0da080113e503b556bb0f38cbe5c
raon37910 <raon37910@gmail.com>
2025-01-06 10:52:31 +0900

    Title: 구역 청소, Time: 80ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250106/구역 청소/district-cleaning.java
@@ -0,0 +1,31 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+    public static boolean[] visited = new boolean[101];
+
+    public static void main(String[] args) throws Exception {
+        for(int i = 0; i < 2; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            visit(start, end);
+        }
+        
+        int cnt = 0;
+        for(int i = 0; i <= 100; i++) {
+            if(visited[i]) cnt++;
+        }
+
+        System.out.println(cnt-1);
+    }
+
+    public static void visit(int start, int end) {
+        for(int i = start; i <= end; i++)
+            visited[i] = true;
+    }
+}
