6b3f41c6df15c80b3bc055fff6607e764655b941
raon37910 <raon37910@gmail.com>
2025-01-06 10:48:54 +0900

    Title: 두 직사각형, Time: 76ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250106/두 직사각형/two-rectangles.java
@@ -0,0 +1,30 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int x1 = Integer.parseInt(st.nextToken());
+        int y1 = Integer.parseInt(st.nextToken());
+        int x2 = Integer.parseInt(st.nextToken());
+        int y2 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int a1 = Integer.parseInt(st.nextToken());
+        int b1 = Integer.parseInt(st.nextToken());
+        int a2 = Integer.parseInt(st.nextToken());
+        int b2 = Integer.parseInt(st.nextToken());
+
+        // 겹치지 않는 경우
+        if(x2 < a1 || x1 > a2 || y2 < b1 || y1 > b2) {
+            System.out.println("nonoverlapping");
+        } else {
+            System.out.println("overlapping");
+        }
+    }
+}
