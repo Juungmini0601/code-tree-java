2035aebbd260f6a37df8c5d5432bc74bce3c1f85
raon37910 <raon37910@gmail.com>
2025-01-09 13:04:27 +0900

    Title: 연속된 숫자 만들기 2, Time: 85ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250109/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
@@ -0,0 +1,31 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringTokenizer st;
+    public static int[] arr = new int[3];
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        
+        for(int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int ans = 0;
+        
+        // Case1 2명의 사람만 연속인 경우
+        if(Math.abs(arr[0] - arr[1]) == 1 || Math.abs(arr[2] - arr[1]) == 1 || Math.abs(arr[2] - arr[0]) == 1) {
+            ans = 1;
+        }
+        // Case2 3명의 사람이 연속이 아닌 경우
+        else {
+            ans = 2;
+        }
+
+        System.out.println(ans);
+    }
+}
