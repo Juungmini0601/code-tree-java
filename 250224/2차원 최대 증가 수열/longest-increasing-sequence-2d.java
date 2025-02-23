c5df5c33406a23eabdbaab72e2b8b23bb8cab0d9
yama <jungmini0601@gmail.com>
2025-02-24 04:55:35 +0900

    Title: 2차원 최대 증가 수열, Time: 233ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 250224/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
+++ 250224/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -11,16 +11,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		init();
-		dp[0][0] = 1;
+		dp[0][0] = 1; // 시작점의 점프 횟수 정의
 
 		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				find(i, j);
+			for (int j = 0; j < m; j++) {
+				find(i, j); // 점프 가능한 지점들 계산
 			}
 		}
 
 		int ans = 0;
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) { // 최대 점프 횟수 계산
 			for (int j = 0; j < m; j++) {
 				ans = Math.max(ans, dp[i][j]);
 			}
@@ -30,10 +30,10 @@ public class Main {
 	}
 
 	public static void find(int row, int col) {
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) {
-				if (arr[i][j] < arr[row][col]) {
-					dp[row][col] = Math.max(dp[row][col], dp[i][j] + 1);
+		for (int i = row + 1; i < n; i++) {  // 아래쪽 방향 점프
+			for (int j = col + 1; j < m; j++) {  // 오른쪽 방향 점프
+				if (arr[row][col] < arr[i][j]) {  // 현재 값보다 커야 점프 가능
+					dp[i][j] = Math.max(dp[i][j], dp[row][col] + 1);  // dp 갱신
 				}
 			}
 		}
@@ -52,3 +52,4 @@ public class Main {
 		}
 	}
 }
+
