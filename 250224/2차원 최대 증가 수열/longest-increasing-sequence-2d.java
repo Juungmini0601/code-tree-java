25067d20bb493e4927ae49110c659903779d41a9
yama <jungmini0601@gmail.com>
2025-02-24 04:50:55 +0900

    Title: 2차원 최대 증가 수열, Time: 167ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 250224/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static int n;
+	public static int m;
+	public static int[][] arr;
+	public static int[][] dp;
+
+	public static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		init();
+		dp[0][0] = 1;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				find(i, j);
+			}
+		}
+
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				ans = Math.max(ans, dp[i][j]);
+			}
+		}
+
+		System.out.println(ans);
+	}
+
+	public static void find(int row, int col) {
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				if (arr[i][j] < arr[row][col]) {
+					dp[row][col] = Math.max(dp[row][col], dp[i][j] + 1);
+				}
+			}
+		}
+	}
+
+	public static void init() {
+		n = sc.nextInt();
+		m = sc.nextInt();
+		arr = new int[n][m];
+		dp = new int[n][m];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+	}
+}
