88830a89d1911793a770fc00837716f6daa70257
yama <jungmini0601@gmail.com>
2025-03-03 13:07:21 +0900

    Title: 두 수의 합, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250303/두 수의 합/sum-of-two-num.java
+++ 250303/두 수의 합/sum-of-two-num.java
@@ -1,13 +1,18 @@
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
 
 /**
  * N개의 정수가 입력으로 주어지고 서로 다른 위치에 있는 두 수를 뽑아 더했을 때 K가 되는 가지 수를 구하시오.
+ * 아 이거 수가 중복으로 들어올 수 있네?
+ *
+ * 숫자가 중복으로 들어올 수 있으려면 어떻게 해야 하지?...
+ * 리스트로 받거나 맵에서 카운트를 증가 시키거나
+ * 그런데 리스트로 받으면 이중탐색해야 해서 아니거든? 그럼 일단 맵으로 카운트를 증가시킨다?
  */
 public class Main {
 	public static int n, k;
-	public static Set<Integer> set = new HashSet<>();
+	public static Map<Integer, Integer> map = new HashMap<>();
 
 	public static Scanner sc = new Scanner(System.in);
 
@@ -17,15 +22,14 @@ public class Main {
 
 		for (int i = 0; i < n; i++) {
 			int value = sc.nextInt();
-			set.add(value);
+			map.put(value, map.getOrDefault(value, 0) + 1);
 		}
 
 		int cnt = 0;
 
-		for (Integer value : set) {
-			if (set.contains(Math.abs(k - value))) {
-				cnt++;
-			}
+		for (Integer value : map.keySet()) {
+			int temp = Math.abs(k - value);
+			cnt += map.get(value) * map.getOrDefault(temp, 0);
 		}
 
 		System.out.println(cnt / 2);
