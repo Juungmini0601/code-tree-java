9b8ce24b8edddd95ca3a14975c55a18abc569b99
yama <jungmini0601@gmail.com>
2025-03-03 13:16:13 +0900

    Title: 두 수의 합, Time: 787ms, Memory: 35MB, Status: Passed - CodeTree

--- 250303/두 수의 합/sum-of-two-num.java
+++ 250303/두 수의 합/sum-of-two-num.java
@@ -28,16 +28,16 @@ public class Main {
 		}
 
 		int ans = 0;
-		
-		for(int i = 0 ; i < n; i++) {
+
+		for (int i = 0; i < n; i++) {
 			long diff = k - arr[i];
-			
-			if(map.containsKey(diff)) {
+
+			if (map.containsKey(diff)) {
 				ans += map.get(diff);
 			}
-			
+
 			// 현재 숫자의 개수를 하나 증가시켜준다.
-			map.put(arr[i], map.getOrDefault(arr[i], +1));
+			map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
 		}
 
 		System.out.println(ans);
