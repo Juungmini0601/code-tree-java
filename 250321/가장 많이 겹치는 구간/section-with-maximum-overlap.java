ac2e8edc4f3113689a72ecae7faf7c195139ac1b
raon <jungmini0601@gmail.com>
2025-03-21 19:38:37 +0900

    Title: 가장 많이 겹치는 구간, Time: 3025ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

---
+++ 250321/가장 많이 겹치는 구간/section-with-maximum-overlap.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static int n;
+
+	public static List<Pos> posList = new ArrayList<>();
+	public static Scanner sc = new Scanner(System.in);
+
+	static class Pos implements Comparable<Pos>{
+		int x;
+		int v;
+
+		public Pos(int x, int v) {
+			this.x = x;
+			this.v = v;
+		}
+
+		@Override
+		public int compareTo(Pos o) {
+			return this.x - o.x;
+		}
+	}
+
+	public static void main(String[] args) {
+		input();
+		int min = posList.get(0).x;
+		int max = posList.get(posList.size() - 1).x;
+		int ans = 0;
+
+		for (int i = min; i <= max; i++) {
+			int cnt = 0;
+
+			for (Pos pos : posList) {
+				if (pos.x > i) {
+					break;
+				}
+
+				cnt += pos.v;
+			}
+
+			ans = Math.max(ans, cnt);
+		}
+
+		System.out.println(ans);
+	}
+
+	public static void input() {
+		n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			int start = sc.nextInt();
+			int end = sc.nextInt();
+
+			posList.add(new Pos(start, +1));
+			posList.add(new Pos(end, -1));
+		}
+
+		Collections.sort(posList);
+	}
+}
