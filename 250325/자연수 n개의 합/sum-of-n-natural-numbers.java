e77e698ef63a09823bdcfec28f7017d3d5463b44
raon <jungmini0601@gmail.com>
2025-03-25 08:23:16 +0900

    Title: 자연수 n개의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250325/자연수 n개의 합/sum-of-n-natural-numbers.java
+++ 250325/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
-	public static Scanner sc = new Scanner(System.in);
-
+	
+	public Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		long s = sc.nextLong();
 		long left = 0;
