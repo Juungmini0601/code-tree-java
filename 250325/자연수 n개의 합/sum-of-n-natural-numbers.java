386f5a514be027260d31edfd2b4ea9a791f8ed57
raon <jungmini0601@gmail.com>
2025-03-25 08:21:33 +0900

    Title: 자연수 n개의 합, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250325/자연수 n개의 합/sum-of-n-natural-numbers.java
+++ 250325/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -1,35 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static Scanner sc = new Scanner(System.in);
-	public static long s;
 
 	public static void main(String[] args) {
-		s = sc.nextLong();
+		long s = sc.nextLong();
 		long left = 0;
-		long right =  s;
-		long min = Long.MIN_VALUE;
-		// sum >= s 보다 작거나 같은 값중 최대 값을 찾기
-		while(left <= right) {
-			long mid = (left + right) / 2;
-			long sum;
-			if (mid % 2 == 0) {
-				sum = (mid / 2) * (mid + 1);
-			} else {
-				sum = mid * ((mid + 1) / 2);
-			}
+		long right = s;
+		long answer = 0;
 
+		while (left <= right) {
+			long mid = (left + right) / 2;
+			long sum = mid * (mid + 1) / 2;
 
 			if (sum > s) {
 				right = mid - 1;
 			} else {
+				answer = mid;
 				left = mid + 1;
-				min = Math.max(min, mid);
 			}
 		}
 
-		System.out.println(min);
+		System.out.println(answer);
 	}
 }
-
