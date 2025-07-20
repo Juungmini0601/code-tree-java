f97a6df91a7f45b3e5af4ad586670a0ef3257f95
Juungmini0601 <jungmini0601@gmail.com>
2025-07-21 08:43:08 +0900

    Title: 양수 직사각형의 최대 크기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250721/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int n, m;
+    public static int[][] grid;
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        input();
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                for(int k = i; k < n; k++) {
+                    for(int l = j; l < n; l++) {
+                        if (isPositiveSquare(i, j, k, l)) {
+                            ans = Math.max(ans, (k - i + 1) * (l - j + 1));
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    public static void input() {
+        n = sc.nextInt();
+        m = sc.nextInt();
+        grid = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+    }
+
+    public static boolean isPositiveSquare(int i, int j, int k, int l) {
+        for(int r = i; r <= k; r++) {
+            for(int c = j; c <= l; c++) {
+                if (grid[r][c] <= 0) return false;
+            }
+        }
+
+        return true;
+    }
+}
