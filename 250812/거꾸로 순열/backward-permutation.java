4ee2f84599c03f0f4d057cffe58705007550de87
Juungmini0601 <jungmini0601@gmail.com>
2025-08-12 09:32:47 +0900

    Title: 거꾸로 순열, Time: 1138ms, Memory: 36MB, Status: Passed - Codetree

---
+++ 250812/거꾸로 순열/backward-permutation.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static final int MAX_N = 8;
+
+    public static int n;
+    public static boolean[] visited = new boolean[MAX_N + 1];
+    public static List<Integer> selected = new ArrayList<>();
+
+    public static void getPermutation(int cnt) {
+        if(cnt == n) {
+            for(int i = 0; i < selected.size(); i++) {
+                System.out.print(selected.get(i) + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = n; i >= 1; i--) {
+            if(visited[i]) continue;
+            visited[i] = true;
+            selected.add(i);
+
+            getPermutation(cnt + 1);
+
+            selected.remove(selected.size() - 1);
+            visited[i] = false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        getPermutation(0);
+    }
+}
